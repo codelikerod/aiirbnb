49c7bd4e5dc5ce38a353e2666ef868738c7568af
Rod <codelikerod@gmail.com>
2017-03-17 18:52:40 +0000

    Modification controller Photos

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
     
 before_action :set_room, only: [:show, :edit, :update] 
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
 
    def index
       @rooms = current_user.rooms 
@@ -14,21 +15,35 @@ before_action :authenticate_user!, except: [:show]
    def create
       @room = current_user.rooms.build(room_params) 
       if @room.save
-          redirect_to @room, notice:"Votre annonce a été ajouté avec succès"
+          if params[:images]
+              params[:images].each do |i|
+                  @room.photos.create(image: i)
+              end
+          end
+          @photos = @room.photos
+          redirect_to edit_room_path(@room), notice:"Votre annonce a été ajouté avec succès"
       else
           render :new
       end
    end
    
    def show
+       @photos = @room.photos
    end
    
    def edit
+       @photos = @room.photos
    end
    
    def update
        if @room.update(room_params)
-           redirect_to @room, notice:"Modification enregistrée..."
+           if params[:images]
+              params[:images].each do |i|
+                  @room.photos.create(image: i)
+              end
+          end
+          @photos = @room.photos
+          redirect_to edit_room_path(@room), notice:"Modification enregistrée..."
        else
            render :edit
        end
@@ -43,6 +58,14 @@ private
        params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room,
        :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo,
        :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active) 
+    end
+    
+    def require_same_user
+       if current_user.id != @room.user_id
+           flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+           redirect_to root_path
+       end
+   end
     end
    
     

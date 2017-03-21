8cf7f738e3aa067dde34b359d09f69982345be2c
Rod <codelikerod@gmail.com>
2017-03-21 14:31:55 +0000

    Création controller reservations

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,16 @@
+class ReservationsController < ApplicationController
+    
+    before_action :authenticate_user!
+    
+    def create
+       @reservation = current_user.reservations.create(reservation_params) 
+       redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
+    end
+    
+    
+   private
+   def reservation_params
+      params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id) 
+   end
+    
+end

21c5a6d36be00ae1cf2caa0e3f40eb1831832014
Rod <codelikerod@gmail.com>
2017-03-22 10:03:18 +0000

    Ajout annonces sur ma page utilisateur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
    
    def show
-      @user = User.find(params[:id]) 
+      @user = User.find(params[:id])
+      @rooms = @user.rooms
    end
     
 end

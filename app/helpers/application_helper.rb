eb5c5a2cf903ebe8e6bdf89bdc586800213d91e0
Rod <codelikerod@gmail.com>
2017-03-24 13:53:44 +0000

    Création bouton fb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
     
     def avatar_url(user)
+        if user.image
+            user.image
+        else
            if user.avatar.present?
                user.avatar.url
            else
                'default_image.png'
            end
+        end
     end
     
 end
